e11c8a58bf9f27266127d00cfb137773665f43cd
bronekT <129657041+bronekT@users.noreply.github.com>
2023-06-03 21:16:22 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,45 @@
+
+var  output1 =  Math.round(Math.random() * 5)+1;
+var  output2 =  Math.round(Math.random() * 5)+1;
+
+if (output1 === 1){
+    document.querySelector(".img1").setAttribute("src" , "images/dice1.png");
+} else if (output1 === 2){
+    document.querySelector(".img1").setAttribute("src" , "images/dice2.png");
+} else if (output1 === 3){
+    document.querySelector(".img1").setAttribute("src" , "images/dice3.png");
+} else if (output1 === 4){
+    document.querySelector(".img1").setAttribute("src" , "images/dice4.png");
+} else if (output1 ===5){
+    document.querySelector(".img1").setAttribute("src" , "images/dice5.png");
+} else {
+    document.querySelector(".img1").setAttribute("src" , "images/dice6.png");
+} 
+
+
+
+if (output2 === 1){
+    document.querySelector(".img2").setAttribute("src" , "images/dice1.png");
+} else if (output2 === 2){
+    document.querySelector(".img2").setAttribute("src" , "images/dice2.png");
+} else if (output2 === 3){
+    document.querySelector(".img2").setAttribute("src" , "images/dice3.png");
+} else if (output2 === 4){
+    document.querySelector(".img2").setAttribute("src" , "images/dice4.png");
+} else if (output2 ===5){
+    document.querySelector(".img2").setAttribute("src" , "images/dice5.png");
+} else {
+    document.querySelector(".img2").setAttribute("src" , "images/dice6.png");
+} 
+
+if (output1 > output2) {
+    document.querySelector(".winner").textContent = "Player 1 WIN";
+} else if (output1 < output2) {
+    document.querySelector(".winner").textContent = "Player 2 WIN";
+} else {
+    document.querySelector(".winner").textContent = " Same Score ";
+}
+
+
+
+
