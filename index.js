e339034cc717996a327a98e599f5895c641edd08
bronekT <129657041+bronekT@users.noreply.github.com>
2023-06-03 21:40:31 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,33 +3,33 @@ var  output1 =  Math.round(Math.random() * 5)+1;
 var  output2 =  Math.round(Math.random() * 5)+1;
 
 if (output1 === 1){
-    document.querySelector(".img1").setAttribute("src" , "images/dice1.png");
+    document.querySelector(".img1").setAttribute("src" , "dice1.png");
 } else if (output1 === 2){
-    document.querySelector(".img1").setAttribute("src" , "images/dice2.png");
+    document.querySelector(".img1").setAttribute("src" , "dice2.png");
 } else if (output1 === 3){
-    document.querySelector(".img1").setAttribute("src" , "images/dice3.png");
+    document.querySelector(".img1").setAttribute("src" , "dice3.png");
 } else if (output1 === 4){
-    document.querySelector(".img1").setAttribute("src" , "images/dice4.png");
+    document.querySelector(".img1").setAttribute("src" , "dice4.png");
 } else if (output1 ===5){
-    document.querySelector(".img1").setAttribute("src" , "images/dice5.png");
+    document.querySelector(".img1").setAttribute("src" , "dice5.png");
 } else {
-    document.querySelector(".img1").setAttribute("src" , "images/dice6.png");
+    document.querySelector(".img1").setAttribute("src" , "dice6.png");
 } 
 
 
 
 if (output2 === 1){
-    document.querySelector(".img2").setAttribute("src" , "images/dice1.png");
+    document.querySelector(".img2").setAttribute("src" , "dice1.png");
 } else if (output2 === 2){
-    document.querySelector(".img2").setAttribute("src" , "images/dice2.png");
+    document.querySelector(".img2").setAttribute("src" , "dice2.png");
 } else if (output2 === 3){
-    document.querySelector(".img2").setAttribute("src" , "images/dice3.png");
+    document.querySelector(".img2").setAttribute("src" , "dice3.png");
 } else if (output2 === 4){
-    document.querySelector(".img2").setAttribute("src" , "images/dice4.png");
+    document.querySelector(".img2").setAttribute("src" , "dice4.png");
 } else if (output2 ===5){
-    document.querySelector(".img2").setAttribute("src" , "images/dice5.png");
+    document.querySelector(".img2").setAttribute("src" , "dice5.png");
 } else {
-    document.querySelector(".img2").setAttribute("src" , "images/dice6.png");
+    document.querySelector(".img2").setAttribute("src" , "dice6.png");
 } 
 
 if (output1 > output2) {
